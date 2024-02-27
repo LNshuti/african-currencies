4c7cd629ce1e60a6ac73a5b1f0be2dff0017c0da
Leonce Nshuti <nshutl0@sewanee.edu>
2024-02-27 09:36:14 -0600

    minor update

--- poverty.py
+++ poverty.py
@@ -13,9 +13,20 @@ print(df.head())
 # The unnamed_67 Column: All entries in the unnamed_67 column are missing. 
 
 # Filtering the dataset for African countries
-africa_pvrty_df = df[df['country_name'].str.contains("Africa")]
+africa_pvrty_df = df[df['country_name'].str.contains("Sub-Saharan Africa")]
 
 # Analyzing missing values by column in the filtered dataset
 missing_values_summary = africa_pvrty_df.isnull().sum()
 
 print(missing_values_summary)
+
+
+# Remove the unnamed_67 column
+africa_pvrty_df = africa_pvrty_df.drop(columns=['unnamed:_67'])
+
+# Display the first few rows of the filtered dataset
+print(africa_pvrty_df.tail())
+
+# Convert from wide to long 
+africa_pvrty_df = africa_pvrty_df.melt(id_vars=['country_name', 'country_code', 'indicator_name', 'indicator_code'], var_name='year', value_name='poverty_rate') 
+africa_pvrty_df.head()
